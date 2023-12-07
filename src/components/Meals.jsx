2c3ccf8410c748752e35dcedc8ce3e2b342f0323
Bogdan Mihalache <bogdan.mihalache@calosense.com>
2023-12-07 14:54:52 +0200

    Error catch

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -11,11 +11,15 @@ export default function Meals() {
   } = useHttp('http://localhost:3000/meals', requestConfig, []);
 
   if (isLoading) {
-    return <p>Fetching meals...</p>
+    return <p className='center'>Fetching meals...</p>;
+  }
+
+  if (error) {
+    return <Error title='Failed to fetch meals' message={error} />;
   }
 
   return (
-    <ul id="meals">
+    <ul id='meals'>
       {loadedMeals.map((meal) => (
         <MealItem key={meal.id} meal={meal} />
       ))}
