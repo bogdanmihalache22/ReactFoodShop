34a627aadedf953dbf9182cff4eab2ff07255752
Bogdan Mihalache <bogdan.mihalache@calosense.com>
2023-12-07 13:45:09 +0200

    Custom Http

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,24 +1,18 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import useHttp from "../hooks/useHttp";
 import MealItem from "./MealItem";
 
-export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+const requestConfig = {};
 
-    useEffect(() => {
-        async function fetchMeals() {
-            const response = await fetch('http://localhost:3000/meals');
-        
-            if (!response) {
-              console.error("Error fetching meals");
-            }
-        
-            const meals = await response.json();
-            setLoadedMeals(meals);
-          }
-        
-          fetchMeals();
-    }, []);
+export default function Meals() {
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp('http://localhost:3000/meals', requestConfig, []);
+
+  if (isLoading) {
+    return <p>Fetching meals...</p>
+  }
 
   return (
     <ul id="meals">
