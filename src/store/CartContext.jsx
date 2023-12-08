9c5ed5267104d4e700bf8ec60c0d4e0451aaab82
Bogdan Mihalache <bogdan.mihalache@calosense.com>
2023-12-08 10:32:31 +0200

    Custom hook for checkout

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -4,6 +4,7 @@ const CartContext = createContext({
   items: [],
   addItem: (item) => {},
   removeItem: (id) => {},
+  clearCart: () => {},
 });
 
 function cartReducer(state, action) {
@@ -46,12 +47,15 @@ function cartReducer(state, action) {
     return { ...state, items: updatedItems };
   }
 
+  if (action.type === "CLEAR_CART") {
+    return { ...state, items: [] };
+  }
+
   return state;
 }
 
 export function CartContextProvider({ children }) {
   const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
-
 
   function addItem(item) {
     dispatchCartAction({ type: "ADD_ITEM", item });
@@ -61,15 +65,19 @@ export function CartContextProvider({ children }) {
     dispatchCartAction({ type: "REMOVE_ITEM", id });
   }
 
+  function clearCart() {
+    dispatchCartAction({ type: "CLEAR_CART" });
+  }
+
   const cartContext = {
     items: cart.items,
     addItem,
-    removeItem
+    removeItem,
+    clearCart,
   };
 
+  console.log(cartContext);
 
-  console.log(cartContext)
-  
   return (
     <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
   );
